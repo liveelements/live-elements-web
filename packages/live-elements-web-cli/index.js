4af15d126deeae3053bdf0c0bf318896b320fa6b
Dinu SV <mail@dinusv.com>
2022-06-12 22:11:55 +0200

    Updated version.

--- packages/live-elements-web-cli/index.js
+++ packages/live-elements-web-cli/index.js
@@ -9,7 +9,7 @@ const convertCommand = require('./commands/convert')
 program
     .name('lvweb')
     .description('Live Elements Web CLI')
-    .version('0.1.0')
+    .version('0.1.2')
 
 program.command('generate <template>')
     .description('Generate a project from a template.')
