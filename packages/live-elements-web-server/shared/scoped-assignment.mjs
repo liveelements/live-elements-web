c83eb0e128fcd3d0bfd41694575b2d29b0bb7fdc
Dinu SV <mail@dinusv.com>
2024-08-27 22:10:44 +0300

    live-elements-web-server: Added ScopedAssignment.populateViewComponentStyles.

--- packages/live-elements-web-server/shared/scoped-assignment.mjs
+++ packages/live-elements-web-server/shared/scoped-assignment.mjs
@@ -10,8 +10,31 @@ export default class ScopedAssignment{
         if ( c.use ){
             for ( let i = 0; i < c.use.length; ++i ){
                 if ( typeof c.use[i] === 'function' && c.use[i].name )
-                ScopedAssignment.populateViewComponent(componentCollection, c.use[i])
+                    ScopedAssignment.populateViewComponent(componentCollection, c.use[i])
             }
         }
+    }
+
+    static populateViewComponentStyles(scopedStyles, c, assertionSupport){
+        if ( scopedStyles ){
+            if ( assertionSupport ){
+                if ( scopedStyles.renderProperties.name !== c.name ){
+                    return { value: undefined, error: `${scopedStyles.renderProperties.name} !== ${c.name}` }
+                }
+            }
+            c.renderProperties = scopedStyles.renderProperties
+        } else { 
+            return { value: undefined, error: `Undefined style for ${c.name}` }
+        }
+        if ( c.use ){
+            for ( let i = 0; i < c.use.length && i < scopedStyles.use.length; ++i ){
+                if ( typeof c.use[i] === 'function' && c.use[i].name ){
+                    const result = ScopedAssignment.populateViewComponentStyles(scopedStyles.use[i], c.use[i], assertionSupport)
+                    if ( !result.value )
+                        return result
+                }
+            }
+        }
+        return { value: true }
     }
 }
