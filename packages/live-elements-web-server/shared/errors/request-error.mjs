0e4ececb4eed30386b30e981549de967cc04fcb3
Dinu SV <mail@dinusv.com>
2025-08-31 23:35:13 +0300

    live-elements-web-server: Added RequestError data log.

--- packages/live-elements-web-server/shared/errors/request-error.mjs
+++ packages/live-elements-web-server/shared/errors/request-error.mjs
@@ -1,21 +1,34 @@
 import StandardError from "./standard-error.mjs"
 
 export default class RequestError extends StandardError{
-    constructor(request, internal){
+    constructor(request, internal, options){
         const url = request.originalUrl
         const method = request.method.toUpperCase()
+
+        const hide = options && options.hasOwnProperty('hide') ? options['hide'] : null
+        const body = request.body && hide !== '*'
+            ? ( !hide 
+                ? Object.entries(request.body).map(([k,v]) => `${k}:${JSON.stringify(v)}`).join(',')
+                : Object.entries(request.body).map(([k,v]) => {
+                    return hide.includes(k) ? `${k}:#` : `${k}:${JSON.stringify(v)}`
+                }).filter(v => v).join(',')
+            )
+            : ''
+
         super(`${method} ${url}`)
 
         this.url = url
         this.method = method
         this.name = this.constructor.name
+        this.body = body
+        
         this._internal = internal
     }
 
     get internal(){ return this._internal }
 
-    static wrap(request, error){
-        return new RequestError(request, error)
+    static wrap(request, error, options){
+        return new RequestError(request, error, options)
     }
 
     static fromJSON(json){
@@ -23,6 +36,6 @@ export default class RequestError extends StandardError{
     }
 
     toString(){
-        return `${this.name}: ${this.message}: \n  ${StandardError.errorToString(this.internal)}`
+        return `${this.name}: ${this.message}: \n ${this.body ? '   Body:' + this.body + ';\n' : ''} ${StandardError.errorToString(this.internal)}`
     }
 }
